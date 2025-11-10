3ea058957040d16696db1d6b49aa5eb4372dcf08
Skellent <skellent69@gmail.com>
2025-11-09 22:57:03 -0400

    Actualizacion de Mermaid

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 // Obtenemos los elementos del HTML
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
-
 // Configura Mermaid con opciones explícitas
 mermaid.initialize({
     startOnLoad: false,  // Desactiva auto-render (lo controlamos manualmente)
-    theme: 'default',
-    flowchart: { 
+    theme: 'dark',
+    look: 'handDrawn',
+    flowchart: {
         useMaxWidth: false,
         htmlLabels: true
     }
 });
-
 function renderMarkdown() {
     const markdownText = editor.value;
     preview.innerHTML = marked.parse(markdownText);
-    
     // Renderiza Mermaid manualmente
     mermaid.run({
         querySelector: '#preview .language-mermaid',
@@ -23,7 +21,6 @@ function renderMarkdown() {
         console.error("Error en Mermaid:", err);
     });
 }
-
 editor.addEventListener('input', renderMarkdown);
 // Render inicial
 renderMarkdown();
